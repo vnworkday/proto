b859779099682e040e1d721b5a8fdb4025942ebc
ntduycs <ntduy.cs@gmail.com>
2024-06-21 15:08:04 +0700

    chore: remove generated code

--- scripts/workflow-check.sh
+++ scripts/workflow-check.sh
@@ -2,16 +2,8 @@
 
 set -euo pipefail
 
-# Find all workflow files in the .github/workflows directory and subdirectories that are added, copied, or modified in the staging area.
-workflows=$(git diff --cached --name-only --diff-filter=ACM | grep -E "^\.github/workflows/.*\.yml$")
-
-if [[ -z $workflows ]]; then
-    echo "✅ No changes in the workflows. Exiting..."
-    exit 0
-fi
-
-for workflow in $workflows; do
-    if $(npm exec action-validator $workflow); then
+for workflow in $(git diff --cached --name-only --diff-filter=ACM | grep -E "^\.github/workflows/.*\.yml$"); do
+    if npm exec action-validator "$workflow"; then
         echo "✅ $workflow is good to go!"
     else
         echo "❌ $workflow is invalid. Please fix the errors and try again."
